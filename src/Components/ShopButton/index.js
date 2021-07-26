325806f6ac45c0bcf5c055dc612d9abf5107c87e
sammiefrog <sammiefrog@live.com>
2021-07-26 14:48:55 -0500

    final cleanup, moved components into jsx files

--- src/Components/ShopButton/index.js
+++ src/Components/ShopButton/index.js
@@ -1,9 +1,1 @@
-import React from "react";
-import ShopIcon from "@material-ui/icons/Shop";
-import Button from "@material-ui/core/Button";
-
-export default function ShopButton({id, name, markAsSold}) {
-    return (
-        <Button variant="outlined" color="secondary" onClick={(e)=> markAsSold(id, name)}><ShopIcon /></Button>
-    );
-}
+export { default } from "./ShopButton.jsx";
